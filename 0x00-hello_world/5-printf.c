a15dff9480148196e2a07427576d4ad15de28f46
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-08 17:35:59 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printsa a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(viod)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+	return (0);
+}
