4c2b7f49a19f057e85474169914319f9e9f00afa
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-08 18:41:55 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text 
+ *
+ * Return: Always 1 (Sucess)
+ */
+int main(void)
+{
+	write(2,"and that peice ofart is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
+
