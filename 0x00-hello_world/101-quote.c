edb7854a4b166c94a4e02ed577ce34f3b0e6f5b3
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-08 18:49:30 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	write(2,"and that peice ofart is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2,"and that peice ofart is useful\" - Dora Korpar,
+		  	2015-10-19\n",59);
 	return (1);
 }
 
