e014fa973fd99b5180ff2b53c0ced017f1dc48ed
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-08 17:45:33 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(viod)
+int main(void)
 {
-	printf("with proper grammer,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
