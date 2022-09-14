fdfa3431e12171d421a24928b1b3b3bf032bcbbc
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-13 17:38:03 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio>
+
+
+/**
+* check the code for addition of two int
+* result: always 0
+*/
+
+int main(void)
+{
+	int i = 10;
+	int k = 11;
+
+	i, k = add(10, 11);
+	printf("%i\n", i, k);
+
+	return (0);
+}
