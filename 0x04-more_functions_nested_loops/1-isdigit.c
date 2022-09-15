6379a963138dff19c7dce77896cd46fa667e33ef
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-15 14:38:28 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,23 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
-/** 
-*isdigit - function that checks for a digit (0 through 9).
-*@c:caracter
-*Return: 1 if c is a digit, 0 otherwise
-*/
+/**
+ *
+ * _isdigit - function that checks for a digit (0 through 9)
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 
-
-
-   int _isdigit(int c)
-
+int _isdigit(int c)
 {
-		if (c >= '0' && c <= '9')
-
-					return (1);
-
-			else
-
-						return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * mul - function that multiplies two integers.
+ * @a:integer
+ * @b:integer
+ * Return: @a and @b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
