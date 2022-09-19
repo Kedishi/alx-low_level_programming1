71073c30cb7db432cda20db105b68df5ffec8c06
Kedishi <Kedishiemmanuel1@gmail.com>
2022-09-19 08:57:47 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,44 +2,38 @@
 
 /**
  * rev_string - prints a reverse string
- *@s: a pointer to an int that will be changed
+ *@s: A pointer to an int that will be changed
  *Return: void
  */
 
 void rev_string(char *s)
 {
+char *start_c, *end_c, c;
+int i, count;
+int length = 0;
 
-	long int i = sizeof(s);
-	char *p = s;
+for (i = 0; s[i]; i++)
+{
+length++;
+}
 
-	printf("lent of p = %ld\n", sizeof(p));
-	while (i >= 0)
+count = length;
+start_c = s;
+end_c = s;
 
-	{
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
 
-		/* printf("i=%d\n",i); */
+for (i = 0; i < count / 2; i++)
+{
 
-		/* printf("siz-i=%ld\n",sizeof(s) - i); */
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+start_c++;
+end_c--;
 
-		/* printf ("%c", s[i--]); */
-
-		*(p + (sizeof(s) - i)) = s[i];
-
-		/* printf("sizeof = %ld \n", sizeof(s)); */
-
-		/* printf("%c\n", s[i]); */
-
-		/* p++; */
-
-		printf("\np=%c\n", *p);
-
-	       printf("size of p = %ld\n", sizeof(p));
-
-		i--;
-	}
-
-	printf("\n");
-
-	/* sleep(2); */
-	/* *s = *p; */
+}
 }
