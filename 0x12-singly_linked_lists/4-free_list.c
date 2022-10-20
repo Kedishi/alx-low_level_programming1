24c67509466c0cbce41ac79b3861f6509df6756f
Kedishi <Kedishiemmanuel1@gmail.com>
2022-10-20 08:50:07 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list
+ * Return:
+ */
+
+void free_list(list_t *head)
+{
+    list_t *current;
+
+    while ((current = head) != NULL)
+    {
+        head = head->next;
+        free(current->str);
+        free(current);
+    }
+}
