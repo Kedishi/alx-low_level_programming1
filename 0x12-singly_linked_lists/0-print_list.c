a56eb2ee654f878e3c703540767e622060b58964
Kedishi <Kedishiemmanuel1@gmail.com>
2022-10-20 08:12:14 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,40 +1,28 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
-/**
- * print_list - prints all the elements of a list_t list
- * @h: is the singly linked list
- * Return: the number of elements in the list
+ * print_list - Prints all the elements of a list_t list
+ * @h: The list_t list
+ * Return: The number of nodes in h
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nodes = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+
+	return (nodes);
 }
