853d538211b7ec0918f7a8c52998865c6a89e6be
Kedishi <Kedishiemmanuel1@gmail.com>
2023-01-06 10:54:23 -0800

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 
+
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
@@ -187,4 +188,4 @@ void shash_table_print_rev(const shash_table_t *ht)
 		printf("'%s': '%s'", node->key, node->value);
 		node = node->sprev;
 		if (node != NULL)
-			pri
+			pr
